2a9f703b47be1d8c75eede65963358ed7c287ec6
0Rick0 <rickrongen@outlook.com>
2016-05-18 11:00:42 +0200

    initial

---
+++ app/src/main/java/nl/ragingmashers/cimsfieldoperations/fiop/ApiManager.java
@@ -0,0 +1,14 @@
+package nl.ragingmashers.cimsfieldoperations.fiop;
+
+import com.google.gson.Gson;
+
+/**
+ * Created by Rick on 18-5-2016.
+ */
+public class ApiManager {
+
+    private Gson gsonFactory(){
+        return null;
+    }
+
+}
